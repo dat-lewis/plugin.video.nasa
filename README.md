c29826f71f6d9b7414a0f2f8cbdeb44c3d2d0b96
Daniel Nimmervoll <daniel@nimmervoll.eu>
2014-07-19 23:26:19 +0200

    init release
    1.0.0

--- README.md
+++
@@ -1,4 +0,0 @@
-plugin.video.nasa
-=================
-
-XBMC video plugin to browse all videos, podcasts and livetv-channels from nasa.gov
--- addon.py
+++ addon.py
@@ -19,68 +19,88 @@
 
 from xbmcswift2 import Plugin, xbmc
 
-
-STRINGS = {
-    'page': 30001,
-    'streams': 30100,
-    'channels': 30101,
-    'title': 30201
-}
-
 STATIC_STREAMS = (
     {
         'title': 'ISS Live Stream',
-        'logo': 'iss.jpg',
+        'logo': 'stream_iss.png',
         'stream_url': ('http://sjc-uhls-proxy.ustream.tv/watch/'
                        'playlist.m3u8?cid=9408562'),
     }, {
          'title': 'ISS HD Earth Viewing Experiment',
-         'logo': 'iss.jpg',
+         'logo': 'stream_iss.png',
          'stream_url': ('http://sjc-uhls-proxy.ustream.tv/watch/'
                         'playlist.m3u8?cid=17074538'),
     }, {
         'title': 'NASA TV: Public Channel',
-        'logo': 'public.png',
+        'logo': 'stream_public.png',
         'stream_url': ('http://public.infozen.cshls.lldns.net/infozen/public/'
                        'public/public_1000.m3u8'),
     }, {
         'title': 'NASA TV: Educational Channel',
-        'logo': 'edu.jpg',
+        'logo': 'stream_education.png',
         'stream_url': ('http://edu.infozen.cshls.lldns.net/infozen/edu/'
                        'edu/edu_1000.m3u8'),
     }, {
         'title': 'NASA TV: Media Channel',
-        'logo': 'media.jpg',
+        'logo': 'stream_media.png',
         'stream_url': ('http://media.infozen.cshls.lldns.net/infozen/media/'
                        'media/media_1000.m3u8'),
     },
 )
 
-YOUTUBE_CHANNELS = (
+YOUTUBE_CHANNELS = (  
     {
-        'name': 'NASA Main',
-        'logo': 'nasa.jpg',
+        'name': 'NASA Main Channel',
+        'logo': 'yt_main.png',
         'user': 'NASAtelevision',
     }, {
-        'name': 'NASA Goddard',
-        'logo': 'goddard.jpg',
-        'user': 'NASAexplorer',
-    }, {
         'name': 'NASA Jet Propulsion Laboratory',
-        'logo': 'jpl.jpg',
+        'logo': 'yt_jpl.png',
         'user': 'JPLnews',
     }, {
         'name': 'NASA Kennedy Space Center',
-        'logo': 'nasa.jpg',
+        'logo': 'yt_ksc.png',
         'user': 'NASAKennedy',
     }, {
-        'name': 'Hubble Space Telescope',
-        'logo': 'hubble.jpg',
-        'user': 'HubbleSiteChannel',
+        'name': 'NASA Science Casts',
+        'logo': 'yt_science_casts.jpg',
+        'user': 'ScienceAtNASA',
+    }, {
+        'name': 'NASA Langley Research Center',
+        'logo': 'yt_langley.png',
+        'user': 'NASALANGLEY',
+    }, {
+        'name': 'NASA Goddard',
+        'logo': 'yt_goddard.png',
+        'user': 'NASAexplorer',
+    }, {
+        'name': 'NASA Marshall Center',
+        'logo': 'yt_marshell.png',
+        'user': 'NASAMarshallTV',
+    }, {
+        'name': 'NASA AMES Research Center',
+        'logo': 'yt_ames.png',
+        'user': 'nasaames',
     }, {
         'name': 'NASA ISS',
-        'logo': 'nasa.jpg',
+        'logo': 'yt_iss.png',
         'user': 'ReelNASA',
+    }, {
+        'name': 'NASA Inside ISS',
+        'logo': 'yt_insideiss.png',
+        'user': 'insideISS',
+    }, {
+        'name': 'NASA Armstrong Flight Research Center',
+        'logo': 'yt_afrc.png',
+        'user': 'DrydenTV',
+    }, {
+        'name': 'NASA X',
+        'logo': 'yt_nasax.jpg',
+        'user': 'NASAXrocks',
+    }, {
+        'name': 'Hubble Space Telescope',
+        'logo': 'yt_hubble.png',
+        'user': 'HubbleSiteChannel',
     },
 )
 
@@ -95,10 +115,12 @@ plugin = Plugin()
 @plugin.route('/')
 def show_root_menu():
     items = [
-        {'label': _('streams'),
-         'path': plugin.url_for('show_streams')},
-        {'label': _('videos'),
-         'path': plugin.url_for('show_channels')},
+        {'label': 'NASA Livestreams',
+         'path': plugin.url_for('show_streams'),
+         'thumbnail': get_logo('root_livestream.png')},
+        {'label': 'NASA YouTube Archives',
+         'path': plugin.url_for('show_channels'),
+         'thumbnail': get_logo('root_youtube.png')},
     ]
     return plugin.finish(items)
 
@@ -126,15 +148,6 @@ def show_channels():
 def get_logo(logo):
     addon_id = plugin._addon.getAddonInfo('id')
     return 'special://home/addons/%s/resources/media/%s' % (addon_id, logo)
-
-
-def _(string_id):
-    if string_id in STRINGS:
-        return plugin.get_string(STRINGS[string_id])
-    else:
-        plugin.log.warning('String is missing: %s' % string_id)
-        return string_id
-
 
 def log(text):
     plugin.log.info(text)
